34468e4ed23e172ca0b599d3a696a5013a0f89fd
bimashazaman <bimashazaman1234@gmail.com>
2022-01-02 14:24:49 +0600

    initial commit

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -2,12 +2,13 @@ import { Avatar, IconButton } from '@material-ui/core'
 import React from 'react'
 import styled from 'styled-components'
 import { AttachFile, SearchOutlined, MoreVert } from '@material-ui/icons'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import MicIcon from '@material-ui/icons/Mic'
 
 function Chat() {
   return (
     <Container>
       <Header>
-        <Avatar />
         <Info>
           <h3>Name</h3>
           <p>last seen at.....</p>
@@ -27,16 +28,45 @@ function Chat() {
           </IconButton>
         </HeaderRight>
       </Header>
+      <Wrap>
+        <Chatbody>
+          <ChatMSG>
+            <p>
+              <Name>
+                <span>
+                  <strong>Name</strong>
+                </span>
+              </Name>
+              this a msg
+              <TimeStamp>{new Date().toLocaleTimeString()}</TimeStamp>
+            </p>
+          </ChatMSG>
+        </Chatbody>
 
-      <Chatbody>
-        <p>
-          <span>
-            <strong>Name</strong>
-          </span>
-          this a msg
-          <TimeStamp>{new Date().toLocaleTimeString()}</TimeStamp>
-        </p>
-      </Chatbody>
+        <ChatReceiver>
+          <ChatMSG>
+            <p>
+              this a msg
+              <TimeStamp>{new Date().toLocaleTimeString()}</TimeStamp>
+            </p>
+          </ChatMSG>
+        </ChatReceiver>
+      </Wrap>
+
+      <ChatFooter>
+        <InsertEmoticonIcon />
+
+        <form>
+          <input
+            // value={input}
+            // onInput={(e) => setInput(e.target.value)}
+            placeholder='Type a message'
+            type='text'
+          />
+          <button type='submit'>Send a message</button>
+        </form>
+        <MicIcon />
+      </ChatFooter>
     </Container>
   )
 }
@@ -52,6 +82,24 @@ const Container = styled.div`
   &:last-child {
     border-bottom: none;
   }
+
+  @media (max-width: 600px) {
+    flex: 1;
+  }
+`
+
+const Wrap = styled.div`
+  background-image: url('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2F1Rg03go.png&f=1&nofb=1');
+  flex: 1;
+  padding: 30px;
+
+  background-size: cover;
+  background-position: center;
+  background-repeat: repeat;
+  /* background-color: #f6f6f6; */
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
 `
 
 const Header = styled.div`
@@ -94,29 +142,81 @@ const HeaderRight = styled.div`
   }
 `
 
-const Chatbody = styled.div`
-  flex: 1;
-  padding: 20px;
-  background-image: url('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2F1Rg03go.png&f=1&nofb=1');
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+const Chatbody = styled.div``
+
+const TimeStamp = styled.span`
+  margin-left: 10px;
+  color: gray;
+  font-size: xx-small;
   background-color: #f6f6f6;
-  overflow-y: scroll;
+`
 
-  &::-webkit-scrollbar {
-    width: 0.5rem;
-  }
-  &::-webkit-scrollbar-track {
-    background: #f6f6f6;
-  }
-  &::-webkit-scrollbar-thumb {
-    background: #888;
+const ChatMSG = styled.div`
+  position: relative;
+  font-size: 16px;
+  padding: 10px;
+  width: fit-content;
+  margin-bottom: 30px;
+  background-color: #ffffff;
+  border-radius: 30px;
+  margin-left: 10px;
+`
+
+const Name = styled.span`
+  font-weight: 800;
+  font-size: xx-small;
+  top: -15px;
+  position: absolute;
+  left: 10px;
+`
+
+const ChatReceiver = styled.div`
+  position: relative;
+  font-size: 16px;
+  padding: 10px;
+  width: fit-content;
+  margin-bottom: 30px;
+  border-radius: 30px;
+  margin-left: 10px;
+  margin-left: auto;
+  /* background-color: #98ff98; */
+
+  & > * {
+    margin-left: auto;
   }
 
-  &::-webkit-scrollbar-thumb:hover {
-    background: #555;
+  & > *:first-child {
+    margin-left: auto;
   }
 `
 
-const TimeStamp = styled.span``
+const ChatFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 62px;
+  padding: 0 20px;
+  border-top: 1px solid lightgray;
+  background-color: #dcdcdc;
+
+  form {
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+    input {
+      flex: 1;
+      border: none;
+      outline: none;
+      font-size: 16px;
+      padding: 10px;
+      border-radius: 10px;
+      margin-left: 10px;
+    }
+
+    button {
+      display: none;
+    }
+  }
+`
